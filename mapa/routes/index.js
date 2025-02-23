3f79bc3cb63e842b5da18b0ccf90eb05fa5651ad
Dunlag <fernando.pinilla85@gmail.com>
2025-02-23 17:52:43 +0100

    solucionado un error de login al cargar el json

--- mapa/routes/index.js
+++ mapa/routes/index.js
@@ -13,29 +13,36 @@ router.get("/login", function (req, res, next) {
   res.sendFile(path.join(__dirname, "../public/login.html"));
 });
 
-// POST: Procesar el formulario de login
 router.post("/login", function (req, res, next) {
   // Obtener email y password del formulario
   const { email, password } = req.body;
 
   // Leer el archivo JSON con los usuarios
-  fs.readFile(
-    path.join(__dirname, "../data/users.json"),"utf8", function (data) {
-      let users;
-      users = JSON.parse(data);
-      // Buscar el usuario que coincida con las credenciales
-      const user = users.find(
-        (u) => u.email === email && u.password === password
-      );
-      if (user) {
-        // Si las credenciales son correctas, redirige a /mapas
-        return res.redirect("/mapas");
-      } else {
-        // Si son incorrectas, se redirige a /login
-        return res.redirect("/login");
-      }
+  fs.readFile(path.join(__dirname, "../data/users.json"), "utf8", function (err, data) {
+    if (err) {
+      console.error("Error al leer users.json:", err);
+      return res.status(500).send("Error interno del servidor");
     }
-  );
+    
+    let users;
+    try {
+      users = JSON.parse(data);
+    } catch (parseErr) {
+      console.error("Error al parsear JSON:", parseErr);
+      return res.status(500).send("Error interno del servidor");
+    }
+
+    // Buscar el usuario que coincida con las credenciales
+    const user = users.find(u => u.email === email && u.password === password);
+    if (user) {
+      // Si las credenciales son correctas, redirige a /mapas
+      return res.redirect("/mapas");
+    } else {
+      // Si son incorrectas, se redirige a /login
+      return res.redirect("/login");
+    }
+  });
 });
+
 
 module.exports = router;
